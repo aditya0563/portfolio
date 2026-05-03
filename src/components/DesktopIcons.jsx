199d075125d6e0bd900224ee5b4bacddbc1d3379
Aditya Thakur <aditya@example.com>
2026-05-04 00:10:00 +0530

    Complete implementation of DesktopIcons.jsx

--- src/components/DesktopIcons.jsx
+++ src/components/DesktopIcons.jsx
@@ -36,3 +36,42 @@ export default function DesktopIcons({ onOpenResume, onOpenDoc, onOpenLeetCode, 
     },
     { 
       id: 2, 
+      name: "Projects", 
+      icon: "https://upload.wikimedia.org/wikipedia/commons/d/d6/Files_App_icon_iOS.png",
+      action: onOpenProject
+    },
+    { 
+      id: 3, 
+      name: "Documents", 
+      icon: "https://upload.wikimedia.org/wikipedia/commons/d/d6/Files_App_icon_iOS.png" ,
+      action: onOpenDoc
+    },
+    { 
+      id: 4, 
+      name: "LeetCode", 
+      icon: "https://cdn.iconscout.com/icon/free/png-256/free-leetcode-logo-icon-svg-download-png-2944960.png",
+      action: onOpenLeetCode
+    },
+    { 
+      id: 5, 
+      name: "Codechef", 
+      icon: "https://img.icons8.com/color/512/codechef.png",
+      action: onOpenCodechef
+    }
+  ];
+
+  const startX = window.innerWidth - 120;
+  const startYOffset = 15;
+
+  return (
+    <div className="fixed inset-0 pt-12 pb-20 pointer-events-none z-20">
+      {items.map((item, index) => (
+        <DraggableIcon
+          key={item.id}
+          item={item}
+          defaultPos={{ x: startX, y: startYOffset + index * 120 }}
+        />
+      ))}
+    </div>
+  );
+}
