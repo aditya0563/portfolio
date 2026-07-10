27ca38775d8e00c743aefd509f815463242a5aee
Aditya Thakur <aditya@example.com>
2026-07-10 09:56:40 +0530

    Complete implementation of eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -8,3 +8,14 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+  },
+])
